9689368cdf7b3759906aa48fcf32265343698497
Angry Coding <mailidanielifechukwu@gmail.com>
2022-09-22 17:50:28 +0100

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useMoralis } from "react-moralis";
 import Header from "../components/Header";
+import LotteryEntrance from "../components/LotteryEntrance";
 import styles from "../styles/Home.module.css";
 
+const supportedChains = ["31337", "5"];
+
 export default function Home() {
+  const { isWeb3Enabled, chainId } = useMoralis();
   return (
     <div className={styles.container}>
       <Head>
@@ -12,6 +17,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      {isWeb3Enabled ? (
+        <div>
+          {supportedChains.includes(parseInt(chainId).toString()) ? (
+            <div className="flex flex-row">
+              <LotteryEntrance className="p-8" />
+            </div>
+          ) : (
+            <div>{`Please switch to a supported chainId. The supported Chain Ids are: ${supportedChains}`}</div>
+          )}
+        </div>
+      ) : (
+        <div>Please connect to a Wallet</div>
+      )}
     </div>
   );
 }
